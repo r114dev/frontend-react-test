4dcf95d30c100d283cfdc31c7d7169d9d29cd258
오동엽 <daniel.williams.kr@gmail.com>
2023-09-14 14:16:37 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+import regionsJson from "./regions.json";
+import "./style.css";
+
+const App: FC = () => {
+  console.log(regionsJson);
+  return <div></div>;
+};
+
+export default App;
