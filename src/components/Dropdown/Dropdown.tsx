b95057907b6b189f514d864168ba355cd51b85b9
오동엽 <daniel.williams.kr@gmail.com>
2023-09-25 11:09:40 +0900

    add text tag

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -5,12 +5,14 @@ import { DropdownProps } from "./types";
 const Dropdown: FC<DropdownProps> = ({ options }) => {
   return (
     <div data-testid="dropdown">
-      <div data-testid="dropdown-input"></div>
+      <div data-testid="dropdown-input">
+        <span data-testid="dropdown-text"></span>
+      </div>
       <ul data-testid="dropdown-options">
         {options.map((option, index) => {
           return (
             <li key={option.value} data-testid={`dropdown-option-${index}`}>
-              {option.title}
+              <span data-testid="dropdown-text">{option.title}</span>
             </li>
           );
         })}
